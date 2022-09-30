1227e5713ada5a963e9f733e8350688e3f09f60a
Shubham Sharma <sharmashubham850@gmail.com>
2022-09-30 11:45:40 +0530

    Added EntityGraph annotation to findAll

--- src/main/java/com/shubbi/shoppingCart/repository/OrderRepository.java
+++ src/main/java/com/shubbi/shoppingCart/repository/OrderRepository.java
@@ -1,9 +1,18 @@
 package com.shubbi.shoppingCart.repository;
 
 import com.shubbi.shoppingCart.entity.Order;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, UUID> {
+    // Used for Join Fetch query
+    // It instructs query to fetch customer by using join clause and thus avoids n+1 query issue
+    @EntityGraph(attributePaths = {"customer"})
+    List<Order> findAll();
 }
